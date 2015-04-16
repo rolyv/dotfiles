4e0c1ae982a2c6ad832843f13a22e49d9eaae7a8
Roly Vicaria <rolyv19@gmail.com>
2015-04-16 12:21:31 -0400

    Moved emacs init up a level and added some more configuration to it. Also modified .bashrc to comment out some terminal config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,7 @@
 ;; ---------------------
 ;; -- Global Settings --
 ;; ---------------------
-(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (require 'cl)
 (require 'ido)
 (require 'ffap)
@@ -70,8 +70,10 @@
 ;; -- JS Mode configuration --
 ;; ---------------------------
 (load "js-config.el")
-(add-to-list 'load-path "~/.emacs.d/jade-mode") ;; github.com/brianc/jade-mode
+(add-to-list 'load-path "~/.emacs.d/lisp/jade-mode") ;; github.com/brianc/jade-mode
 (require 'sws-mode)
 (require 'jade-mode)    
 (add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
 (add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+
+(shell)
